aa1dee7b0e827f3bb89d689c35cb9ac7ca7831cc
junho.lee <junho86@direa.co.kr>
2022-09-28 20:28:14 +0900

    스ㅏã타또

---
+++ src/main/java/com/devlCafe/DevlCafeApplication.java
@@ -0,0 +1,13 @@
+package com.devlCafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlCafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevlCafeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devlCafe/DevlCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlCafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlCafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
